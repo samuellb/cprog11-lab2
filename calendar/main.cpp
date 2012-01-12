c526b3c44a5ce553592053300adf30575f06f975
Samuel Lidén Borell <samuel@slbdata.se>
2012-01-12 15:47:13 +0100

    Add a few comments in main.cpp

--- calendar/main.cpp
+++ calendar/main.cpp
@@ -27,24 +27,29 @@ template<typename T> int work() {
     calendar.set_format(lab2::Calendar<T>::format::cal);
 
     Menu main_menu("Huvudmeny");
-    Menu format_menu("Formatmeny");
-    Menu event_menu("Händelsemeny");
 
+    // Main menu
     SelectDateMenuItem<T> select_date("Välj datum", calendar);
     main_menu.add(select_date);
 
     SelectMonthMenuItem<T> select_month("Välj månad", calendar);
     main_menu.add(select_month);
 
+    Menu format_menu("Formatmeny");
+    Menu event_menu("Händelsemeny");
     main_menu.add(format_menu);
     main_menu.add(event_menu);
     
+    // Format menu
     SelectFormatMenuItem<T> format_list("Lista", calendar, lab2::Calendar<T>::format::list);
     SelectFormatMenuItem<T> format_calendar("Kalender", calendar, lab2::Calendar<T>::format::cal);
     SelectFormatMenuItem<T> format_ical("ical", calendar, lab2::Calendar<T>::format::icalendar);
     format_menu.add(format_list);
     format_menu.add(format_calendar);
     format_menu.add(format_ical);
+    
+    // Event menu
+    // TODO
 
     CalendarUI<T> ui(calendar);
     UserInterface::set_ui(ui);
